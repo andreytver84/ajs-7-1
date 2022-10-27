3af635ea870c98b4139460dd88d89e5e335080f4
Андрей Увиков <uvikmedium@mail.ru>
2022-10-27 17:26:05 +0300

    done regexp 1st attempt

---
+++ src/js/__tests__/validateName.test.js
@@ -0,0 +1,38 @@
+import Validator from '../app';
+
+test('Expecting result true', () => {
+    const wrongName = [
+      "1aaaa",
+      "aaaa1",
+      "1aaaa",    
+      "-aaaaaa",
+      "aaaaa-",
+      "_aaaaaa",
+      "aaaaa_"   
+    ];
+
+  const rightName =[
+      "aaaa-aaaa",    
+      "aaaa111aaaa",    
+      "aa-2-ssss",
+      "aaa_aaaaa",
+      "aaaaa_2aaaa",
+      "aaaa_333-ssss"
+  ];
+
+  let result = true;
+  rightName.forEach(item => {
+      if (!Validator.validateUsername(item)) {
+          result = false;      
+      }; 
+  }); 
+
+  wrongName.forEach(item => {
+      if (!Validator.validateUsername(item)) {
+          result = false;      
+      }; 
+  }); 
+
+    
+    expect(result).toEqual(true);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -5,23 +5,8 @@ console.log('worked');
 
 console.log(sum([1, 2]));
 
-export default function specAtackGet(obj) {   
-    let specAtackArr;
-    let specAtackArrRes = [];
-    for (let key in obj) {
-        if (typeof obj[key] == "object") {
-            specAtackArr = [...obj[key]];
-            specAtackArr.forEach(item => {
-                let {id, name, description = 'Описание недоступно', icon} = item;
-                specAtackArrRes.push({id,name,description,icon});
-            });
-        }
+export default class Validator {   
+    static validateUsername(name) {
+        return /^[^-\d_]\w+[_\d-]{0,3}\w+[^-\d_]$/.test(name);
     }
-    return specAtackArrRes;    
 };
-
-
-
-
-
-
